007b9068138f71ffcc0f9eb24a0c347ab6307ada
LVooo <1371458538@qq.com>
2023-06-15 22:52:15 +1000

    Add eureka

---
+++ backend/brainoverflow/service_eureka/src/main/java/com/gogohd/eureka/EurekaApplication.java
@@ -0,0 +1,24 @@
+package com.gogohd.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+    @EnableWebSecurity
+    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            super.configure(http);
+        }
+    }
+}
