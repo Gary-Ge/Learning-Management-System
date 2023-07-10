db3eb2bec1cb11a7993a8120c90543658c4aeef9
MoshuJin <100931823+MoshuJin@users.noreply.github.com>
2023-07-10 15:55:30 +1000

    Delete babel.config.js

--- frontend/myapp/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: [
-      ['@babel/plugin-transform-runtime', { corejs: 3 }],
-      ['@babel/plugin-transform-private-methods', { loose: true }]
-    ]
-  };
-  
